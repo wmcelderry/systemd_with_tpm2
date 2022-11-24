e1abe4f0f76e7fb58010e327b93dab88c612526f
Will McElderry <wmcelderry@users.noreply.github.com>
2022-11-24 10:10:39 +0000

    Improve mkcrypttab.sh script.

--- mkcrypttab.sh
+++ mkcrypttab.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-DEV="..."
+DEV="${1}"
+
+if [[ -z "${DEV}" ]]; then
+    #default to use current root.
+    DEV="$(mount  | grep 'on / ' | awk '{print $1;}')"
+    echo "Defaulting to current root device: ${DEV}" 1>&2
+fi
+
 echo "luks $(sudo blkid --output export "${DEV}"  | grep ^UUID) none tpm2-device=auto"

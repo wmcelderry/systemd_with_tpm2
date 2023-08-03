d3788e756d97dccc825998e9340555e03480c263
Benjamin Binier <benjamin.binier@qorvo.com>
2023-08-03 13:47:16 +0200

    Simplify mkcrypttab commands

--- mkcrypttab.sh
+++ mkcrypttab.sh
@@ -5,8 +5,8 @@ volname="${2:-luks}"
 
 if [[ -z "${dev}" ]]; then
     #default to use current root.
-    dev="$(mount  | grep 'on / ' | awk '{print $1;}')"
+    dev=$(awk '{if ( $2 == "/") { print $1}}' /proc/mounts)
     echo "Cryptab: Defaulting to current root device: ${dev}" 1>&2
 fi
 
-echo "${volname} $(sudo blkid --output export "${dev}"  | grep ^UUID) none tpm2-device=auto"
+echo "${volname} $(lsblk -n -o uuid "${dev}") none tpm2-device=auto"
